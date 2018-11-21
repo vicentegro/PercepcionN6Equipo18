4db59514b73be40c9a330c684ec03ef3954ad328
vicentegro <42250297+vicentegro@users.noreply.github.com>
2018-11-21 12:22:25 -0600

    AJUSTES Y COMENTARIOS
    Le agregué algunos comentarios y le hice algunos ajustes

--- proyecto.ino
+++ proyecto.ino
@@ -7,7 +7,7 @@ int echo=2;
 Servo servito; //declara servo
 long distanciaOb;
 int direccionOb;
-long distanciaNueva;
+long distanciaNew;
 long avanceOb;
 int giroOb;
 int reglas={1, }
@@ -21,12 +21,15 @@ pinMode(echo,INPUT);
 
 
 void loop(){
- distanciaOb=medicionSensor();
- direccionOb=deteccion(distanciaOb);
- if (direccionOb > 90){direccionOb=servito.read()-90;}
- else if (direccionOb <90){direccionOb=90-servito.read();}
+ distanciaOb=medicionSensor(); //obtiene dato del sensor
+ int theta=deteccion(distanciaOb); //obtiene posicion del servo si hay objeto a 5 cm de distancia
+ if (theta > 90){direccionOb=servito.read()-90;} //ajusta con respecto al centro
+ else if (theta <90){direccionOb=90-servito.read();}
+ else if(theta ==90){direccionOb=theta;}
+ /*se realiza nueva medicion para saber si el objeto cambió su posición*/
  distanciaNew=medicionSensor(); //nueva medicion
  dirNew=deteccion(distanciaNueva); //nueva deteccion
+ /*NUEVAS VARIABLES*/
  giroOb=dirNew-direccionOb; //objeto se desplaza a un lado
  avanceOb=distanciaNew-distanciaOb; //objeto avanza
  
@@ -36,16 +39,16 @@ void loop(){
 
 
 int deteccion(long x){
-  int theta;
+  int theta; //desplazamiento rotacional del servo
    for(int i=0;i<=180;i++){
     servito.write(i);
-    if(x>=4 && x<=6){theta=servito.read();}
+    if(x>=4 && x<=6){theta=servito.read();} //si encuentra algo entre 4 y 6, el objeto se toma como detectado
     return theta;
     }
     delay(100);
     for(int j=180;jj>=0;j--){
     servito.write(j);
-    if(x>=4 && x<=6){theta=servito.read();}
+    if(x>=4 && x<=6){theta=servito.read();} //si encuentra algo entre 4 y 6, el objeto se toma como detectado
     return theta;
     }
     delay(100);
@@ -58,11 +61,9 @@ long medicionSensor(){
   digitalWrite(trigger,HIGH);
   delayMicroseconds(10); 
   digitalWrite(trigger,LOW);
-  
+  /*------------------*/
   t=pulseIn(echo,HIGH); //mide el tiempo que tarda en salir y regresar el pulso sonico
- 
  //calculo de la distancia: velocidad= 2*distancia/tiempo ya que recorre la distancia que sale y en la que regresa
   d=t/59; (velocidad del sonido 340 m/s se convierte a cm/s)
-  
-  return d;
+  return d; //salida final del sensor
 }
