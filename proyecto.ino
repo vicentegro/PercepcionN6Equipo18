9204261aee84ee55e077fbedb5c9584022235bfc
vicentegro <42250297+vicentegro@users.noreply.github.com>
2018-11-24 12:09:51 -0600

    Ajustes finales
    Le hice unos ajustes al código en su lógica de programación. El código compila y funciona. Le agregué diversos comentarios finales

--- proyecto.ino
+++ proyecto.ino
@@ -1,5 +1,13 @@
-/*CODIGO PRINCIPAL DEL PROYECTO DE PERCEPCION */
-
+/*
+    CODIGO PRINCIPAL DEL PROYECTO DE PERCEPCION N6
+    AGOSTO-DICIEMBRE 2018
+    EQUIPO #18
+    MIEMBROS: 
+    VICENTE GUERRERO
+    ALEJANDRO GUTIERREZ
+    EDER CARRIZALES
+*/
+//se incluyen las siguientes librerías
 #include <Servo.h>
 #include <LEANTEC_ControlMotor.h>
 #include <stdio.h>
@@ -8,30 +16,31 @@
 
 double inputs[2]; //array de entradas para sistema difuso
 int reglas=9; //cantidad de reglas difusas
-double entradas = 2;//NTI=NTV-1;
+double entradas = 2;//cantidad de entradas
 
-int tabla_reglas[9][4]=    {{1,	1,	1,	1},
-                            {1,	2,	2,	2},
-                            {1,	3,	2,	3},
-                            {2,	1,	1,	1},
-                            {2,	2,	2,	2},
-                            {2,	3,	3,	3},
-                            {3,	1,	2,	1},
-                            {3,	2,	3,	2},
-                            {3,	3,	3,	3}}; //tabla de reglas difusas
+//la siguiente es la tabla con las reglas de nuestra lógica difusa:
+int tabla_reglas[9][4]=   {{1,	1,	1,	2},
+{1,	2,	2,	1},
+{1,	3,	3,	2},
+{2,	1,	3,	3},
+{2,	2,	2,	3},
+{2,	3,	1,	3},
+{3,	1,	3,	3},
+{3,	2,	2,	3},
+{3,	3,	1,	3}};
+
+ 
 int trigger=12; //pin de trigger del sensor
 int echo=13; //pin de echo del sensor
-Servo servito; //declara servo
+Servo servito; //declara nuestro servo 
 ControlMotor control(2,3,7,4,5,6); //declara el control para los motores del auto
-long distanciaOb; //variable de distancia del objeto
+double distanciaOb; //variable de distancia del objeto
 int direccionOb; //direccion del objeto
-long distanciaNew,dirNew;
-long avanceOb;
-int giroOb; 
-double giroAuto,velAuto; //salidas del sistema difuso con las que se controla el auto
+double giroAuto,velAuto; //salidas con las que se controla el auto
 
 
 void setup(){
+  Serial.begin(9600);
 servito.attach(11); //definir puerto para servo
 pinMode(trigger,OUTPUT); //define puertos para sensor
 pinMode(echo,INPUT);
@@ -39,48 +48,80 @@ pinMode(echo,INPUT);
 
 
 void loop(){
- distanciaOb=medicionSensor(); //obtiene dato del sensor
- direccionOb=deteccion(distanciaOb); //obtiene posicion del servo si hay objeto a 10 cm de distancia
- 
-  /******************************/
-   inputs[0]=((distanciaOb-5)*(1-0)/(25-5))+0; //normalizacion y guardado en array PARA SISTEMA DIFUSO
-   inputs[1]=((direccionOb-0)*(1-0)/(180-0))+0;
+   double x;
+   for(int i=0;i<=180;i++){ //INICIA ESCANEO HACIA DE 0 A 180°
+    servito.write(i);//mueve poco a poco la flecha del servo
+    x=medicionSensor(); //mide distancia para encontrar objetos
+    if(x>=5 && x<=25){
+      distanciaOb=x;
+      direccionOb=i; //si encuentra algo en una distancia max de 25 cm, el objeto se toma como detectado
+    } else{
+      distanciaOb=0; //FUERA DE NUESTRO RANGO DE DETECCION
+      direccionOb=0;
+    }
+    if(distanciaOb!=0 && direccionOb!=0){
+    inputs[0]=((distanciaOb-5)*(1-0)/(25-5))+0; //normalizacion y guardado en array PARA SISTEMA DIFUSO
+    inputs[1]=((direccionOb-0)*(1-0)/(180-0))+0;
       
    giroAuto= FuzzySysY1(inputs,tabla_reglas);
    velAuto = FuzzySysY2(inputs,tabla_reglas);
    
-   control.Motor(velAuto,giroAuto); //SALIDA FINAL A LOS MOTORES
- 
-}
-           
-
-int deteccion(){
-  long x;
-  int theta; //desplazamiento rotacional del servo
-   for(int i=0;i<=180;i++){
-    servito.write(i);//mueve poco a poco la flecha del servo
-    x=medicionSensor(); //mide distancia para encontrar objetos
-    delay(10);
-    if(x>=5 && x<=25){
-      theta=servito.read(); //si encuentra algo en una distancia max de 25 cm, el objeto se toma como detectado
-      return theta;
-      }
+    } 
+      else {
+      giroAuto=0; //EL AUTO NO SE MUEVE SI NO HAY DETECCION
+      velAuto=0;
     }
-    for(int j=180;j>=0;j--){
+   control.Motor(velAuto*-1,giroAuto);
+   Serial.print("Distancia: ");
+   Serial.println(distanciaOb);
+   Serial.print("Direccion: ");
+   Serial.println(direccionOb);
+   Serial.print("Giro: ");
+   Serial.println(giroAuto);
+   Serial.print("Vel: ");
+   Serial.println(velAuto);
+    
+    }
+     
+   
+    for(int j=180;j>=0;j--){ //ESCANEO DE 180° A 0°
      servito.write(j);
-      x=medicionSensor();
-    delay(10);
-    if(x>=5 && x<=25){
-      theta=servito.read(); //si encuentra algo en una distancia el objeto se toma como detectado
-      return theta;
-      }
+     x=medicionSensor();
+      if(x>=5 && x<=25){
+        distanciaOb=x;
+      direccionOb=j; //si encuentra algo en una distancia el objeto se toma como detectado
+     
+    }else{
+     distanciaOb=0;
+     direccionOb=0;
+    }
+    if(distanciaOb!=0 && direccionOb!=0){
+    inputs[0]=((distanciaOb-5)*(1-0)/(25-5))+0; //normalizacion y guardado en array PARA SISTEMA DIFUSO
+    inputs[1]=((direccionOb-0)*(1-0)/(180-0))+0;
+      
+   giroAuto= FuzzySysY1(inputs,tabla_reglas);
+   velAuto = FuzzySysY2(inputs,tabla_reglas);
+   
+    } else {
+      giroAuto=0;
+      velAuto=0;
+    }
+    control.Motor(velAuto*-1,giroAuto);
+   Serial.print("Distancia: ");
+   Serial.println(distanciaOb);
+   Serial.print("Direccion: ");
+   Serial.println(direccionOb);
+   Serial.print("Giro: ");
+   Serial.println(giroAuto);
+Serial.print("Vel: ");
+   Serial.println(velAuto);
     }
       
-  }
+}   
 
-long medicionSensor(){
-  long t; //timepo que demora en llegar el eco
-  long d;  //distancia a la que está el objeto
+double medicionSensor(){ //FUNCION PARA SENSOR ULTRASONICO
+  double t; //timepo que demora en llegar el eco
+  double d;  //distancia a la que está el objeto
 //envio de pulso de 10us
   digitalWrite(trigger,HIGH);
   delayMicroseconds(10); 
@@ -124,9 +165,9 @@ double Type1FS(double x,int n,double V[]){
     }
   }
   
-double FuzzySysY1(double X[],int DB[][4]) //SISTEMA DE LOGICA DIFUSA PARA SALIDA Y1
+double FuzzySysY1(double X[],int DB[][4]) //SISTEMA DE LOGICA DIFUSA PARA SALIDA Y1: GIRO DEL AUTO
 {
-   double PARAM[3][3]={{0,0.25,0.5} //RANGOS DE LAS FUNCIONES DE MEMBRESIA
+   double PARAM[3][3]={{0,0.25,0.5} //MEDIDAS DE LAS FUNCIONES DE MEMBRESIA
                       ,{0.25, 0.5, 0.75}
                       ,{0.5, 0.75, 1}};
     double V[3];
@@ -155,11 +196,11 @@ double FuzzySysY1(double X[],int DB[][4]) //SISTEMA DE LOGICA DIFUSA PARA SALIDA
          sum1=(sum1+(Fo[r]*AC[DB[r][3]-1]));
          sum2=(sum2+Fo[r]);
          }
-      double y1=(((sum1/sum2)-0)*(100-(-100))/(1-0)))+(-100);     
+      double y1=(((sum1/sum2)-0)*(100-(-100))/(1-0))+(-100);     
       return y1; //SALIDA FINAL Y1 GIRO DEL AUTO
 }
 
- double FuzzySysY2(double X[],int DB[][4]) //SISTEMA DE LOGICA DIFUSA PARA LA SALIDA Y2
+ double FuzzySysY2(double X[],int DB[][4]) //SISTEMA DE LOGICA DIFUSA PARA LA SALIDA Y2: VELOCIDAD DEL AUTO
 {
     double PARAM[3][3]={{0,0.25,0.5} //RANGOS DE LAS FUNCIONES DE MEMBRESIA
                       ,{0.25, 0.5, 0.75}
@@ -192,8 +233,7 @@ double FuzzySysY1(double X[],int DB[][4]) //SISTEMA DE LOGICA DIFUSA PARA SALIDA
          sum3=(sum3+(Fo[s]*AC[DB[s][4]-1]));
          sum4=(sum4+Fo[s]);
          }
-       double y2=(((sum3/sum4)-0)*(254-(-254))/(1-0)))+(-254);
+       double y2=(((sum3/sum4)-(-254))*((254-(-254))/(1-0)))+(-254);
       
       return y2; //SALIDA FINAL Y2 VELOCIDAD DEL AUTO
 }
- 
